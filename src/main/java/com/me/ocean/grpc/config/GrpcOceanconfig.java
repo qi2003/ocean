5b8a162bb7218e98da9d625d50ac3a9444121c45
shang <981654075@qq.com>
2023-02-17 17:31:22 +0800

    oceanservice

--- src/main/java/com/me/ocean/grpc/config/GrpcOceanconfig.java
+++ src/main/java/com/me/ocean/grpc/config/GrpcOceanconfig.java
@@ -1,6 +1,16 @@
 package com.me.ocean.grpc.config;
 
+import io.bbex.admin.AdminApplySymbolServiceGrpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@Component
 public class GrpcOceanconfig {
+
+    public static final String TB_OCEAN_CHANNEL_NAME = "tb-ocean";
 
 
 
